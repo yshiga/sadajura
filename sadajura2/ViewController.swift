bc86d31c77c9701702b7466e3491da70e077b2b5
shiga yuichi <yuichi.shiga@gmail.com>
2015-09-19 18:09:32 -0700

    first commit

---
+++ sadajura2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sadajura2
+//
+//  Created by shiga yuichi on 9/19/15.
+//  Copyright © 2015 whomentors. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
