eda5d611c58fffb15ac35c65b635e2d1654cb09a
shiga yuichi <yuichi.shiga@gmail.com>
2015-09-20 06:53:58 -0700

    add create flight

---
+++ sadajura2/ProfileViewFlightCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewFlightCell.swift
+//  sadajura
+//
+//  Created by shiga yuichi on 9/20/15.
+//  Copyright © 2015 whomentors. All rights reserved.
+//
+
+import Foundation
+import ParseUI
+
+class ProfileViewFlightCell: UITableViewCell {
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
