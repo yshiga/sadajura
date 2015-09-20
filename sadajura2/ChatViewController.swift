8e21ec7d737835ed5607643f53ae17a83472e6be
shiga yuichi <yuichi.shiga@gmail.com>
2015-09-19 20:13:54 -0700

    install chat library

--- sadajura2/ChatViewController.swift
+++ sadajura2/ChatViewController.swift
@@ -1,28 +1,24 @@
 //
-//  ViewController.swift
-//  sadajura2
+//  ChatViewController.swift
+//  sadajura
 //
 //  Created by shiga yuichi on 9/19/15.
 //  Copyright © 2015 whomentors. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class ChatViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let chatView = ChatViewController()
-        presentViewController(chatView, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
